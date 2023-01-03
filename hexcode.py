f638f22487c15e3a5113035e6747c6a354726979
Branch, Christopher <Christopher_Branch@comcast.com>
2023-01-03 12:56:12 -0500

    part c

--- hexcode.py
+++ hexcode.py
@@ -5,9 +5,16 @@ app.config['DEBUG'] = True
 
 # Code the 'valid_hex_chars' function here:
 
-@app.route('/hex_form')
-def hex_form():    
-    return render_template('hex_form.html')
+@app.route('/hex_form', methods=["GET", "POST"])
+def hex_form():
+    if request.method == 'POST':
+        hex = request.form['hex']
+        feedback = "Successful Submission"
+    else:        
+        hex = '00FF00'
+        feedback = "feedback"
+
+    return render_template('hex_form.html', hex=hex, feedback= feedback)
 
 if __name__ == '__main__':
     app.run()
