f67d0708d2bc7286d6d192f970601e065115f3f2
Branch, Christopher <Christopher_Branch@comcast.com>
2023-01-03 16:24:50 -0500

    project part b

--- webcaesar.py
+++ webcaesar.py
@@ -4,9 +4,21 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
-@app.route('/user_message')
-def user_message():    
-    return render_template('user_message.html')
+@app.route('/user_message', methods=["GET", "POST"])
+def user_message(): 
+    original_msg= "The user's message will appear here"
+    coded_msg ="The coded message will appear here"
+
+    if request.method =='POST':
+        original_msg= request.form['message']
+        shift = int(request.form['shift'])
+        code_action = request.form['code-action']
+    elif request.method=='GET':
+        original_message = ''
+        number = ''
+        code_action = ''
+
+    return render_template('user_message.html', original_msg=original_msg, coded_msg=coded_msg)
 
 if __name__ == '__main__':
     app.run()
